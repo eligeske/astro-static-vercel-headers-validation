d1768de492aec35a63006c6f3fa867235f08fd43
Eli Geske <eligeske@gmail.com>
2023-06-07 08:10:42 -0400

    case 1: base astro on vercel static, no header configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,4 @@ import vercel from "@astrojs/vercel/static";
 export default defineConfig({
   output: "static",
   adapter: vercel(),
-  server: {
-    headers: {
-      "X-Custom-Test": "defineConfig -> server.headers",
-    },
-  },
 });
